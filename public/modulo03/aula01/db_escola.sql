3644402d27463cb16918c0bb96a398063a6fded8
israelcena <israelsjm@gmail.com>
2022-06-13 23:03:43 -0300

    include insert values

--- public/modulo03/aula01/db_escola.sql
+++ public/modulo03/aula01/db_escola.sql
@@ -1,20 +1,23 @@
 -- Para criar um banco de dados --
 CREATE DATABASE db_escola;
-
 -- selecionar db --
 USE db_escola;
-
-CREATE TABLE tb_professor
-(
+-- Criar Tabela
+CREATE TABLE tb_professor (
   nome VARCHAR(100) NOT NULL,
   cpf CHAR(11) NOT NULL,
   email VARCHAR(255)
 );
-
-CREATE TABLE tb_aluno
-(
+CREATE TABLE tb_aluno (
   nome VARCHAR(100) NOT NULL,
   cpf CHAR(11) NOT NULL,
   email VARCHAR(255),
   matricula INTEGER(10)
 );
+-- inserir dados
+INSERT INTO tb_professor (nome, email, cpf)
+VALUES (
+    'Alessandro',
+    'alessandro@email.com',
+    '12312312312'
+  );
