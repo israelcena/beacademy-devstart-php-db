4cb1d690dd77ccd77d075fb37da297f4995dbbbb
israelcena <israelsjm@gmail.com>
2022-06-13 22:48:34 -0300

    include sql

---
+++ public/modulo03/aula01/db_escola.sql
@@ -0,0 +1,20 @@
+-- Para criar um banco de dados --
+CREATE DATABASE db_escola;
+
+-- selecionar db --
+USE db_escola;
+
+CREATE TABLE tb_professor
+(
+  nome VARCHAR(100) NOT NULL,
+  cpf CHAR(11) NOT NULL,
+  email VARCHAR(255)
+);
+
+CREATE TABLE tb_aluno
+(
+  nome VARCHAR(100) NOT NULL,
+  cpf CHAR(11) NOT NULL,
+  email VARCHAR(255),
+  matricula INTEGER(10)
+);
