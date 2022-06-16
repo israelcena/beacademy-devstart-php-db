4a66fd15c3a8d61159629613f743ed044591bfe2
israelcena <israelsjm@gmail.com>
2022-06-16 19:46:36 -0300

    include db data

--- src/Controller/AbsController.php
+++ src/Controller/AbsController.php
@@ -4,10 +4,19 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Connection\Connection;
+
 abstract class AbsController
 {
   public function render(string $viewName): void
   {
+
+    $con = Connection::getConnetion();
+
+    $result = $con->prepare('SELECT * FROM tb_category');
+    $result->execute();
+
+    var_dump($result->fetchAll(\PDO::FETCH_ASSOC));
     include "/application/src/View/{$viewName}.php";
   }
 }
