0c22b105d3724c34cb22555f38c348ce72bdfcbc
Israel Cena <israelsjm@gmail.com>
2022-06-17 13:11:40 -0300

    add action buttons

--- src/View/category/list.php
+++ src/View/category/list.php
@@ -1,26 +1,32 @@
+<div class="container">
+  <h1>Listar Categorias</h1>
 
+  <table class="table table-hover table-striped">
+    <thead class="table-dark">
+      <tr>
+        <th>#</th>
+        <th>Nome</th>
+        <th>Descrição</th>
+        <th>Ações</th>
+      </tr>
+    </thead>
 
-<h1>Listar Categorias</h1>
-
-<table class="table table-hover table-striped">
-  <thead class="table-dark">
-    <tr>
-      <th>#</th>
-      <th>Nome</th>
-      <th>Descrição</th>
-    </tr>
-  </thead>
-
-  <tbody>
-    <?php
-    $categories = $data->fetchAll(\PDO::FETCH_ASSOC);
-    foreach ($categories as $category) {
-      echo "<tr>"
-        . "<td>{$category['id']}</td>"
-        . "<td>{$category['name']}</td>"
-        . "<td>{$category['description']}</td>"
-        . "</tr>";
-    }
-    ?>
-  </tbody>
-</table>
+    <tbody>
+      <?php
+      $categories = $data->fetchAll(\PDO::FETCH_ASSOC);
+      foreach ($categories as $category) {
+        extract($category);
+        echo "<tr>"
+          . "<td>{$id}</td>"
+          . "<td>{$name}</td>"
+          . "<td>{$description}</td>"
+          . "<td>
+        <a class='btn btn-sm btn-danger' href='/categorias/deletar?id=$id'>Excluir<a>
+        <a class='btn btn-sm btn-info' href='/categorias/editar?id=$id'>Editar<a>
+        </td>"
+          . "</tr>";
+      }
+      ?>
+    </tbody>
+  </table>
+</div>
