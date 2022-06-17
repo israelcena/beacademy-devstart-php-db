c56c0f0e2367b7cd8db2217c674aa621849b990d
Israel Cena <israelsjm@gmail.com>
2022-06-17 12:42:55 -0300

    add category

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Connection\Connection;
+
 class CategoryController extends AbsController
 {
   public function listAction(): void
@@ -15,6 +17,14 @@ class CategoryController extends AbsController
   }
   public function addAction(): void
   {
+    if ($_POST) {
+      $con = Connection::getConnetion();
+      $data = $con->prepare('INSERT INTO tb_category (name, description) VALUES (:name, :description)');
+      $data->bindValue(':name', $_POST['name']);
+      $data->bindValue(':description', $_POST['description']);
+      $data->execute();
+      header('Location: /categorias');
+    }
     parent::render('category/add');
   }
   public function removeAction(): void
