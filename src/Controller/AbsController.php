e0cc586821402b9c14ebfaf8cd86fe1b838f5829
Israel Cena <israelsjm@gmail.com>
2022-06-17 12:09:33 -0300

    include bootstrap, navbar and data

--- src/Controller/AbsController.php
+++ src/Controller/AbsController.php
@@ -4,19 +4,12 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use App\Connection\Connection;
-
 abstract class AbsController
 {
-  public function render(string $viewName): void
+  public function render(string $viewName, $data = null): void
   {
-
-    $con = Connection::getConnetion();
-
-    $result = $con->prepare('SELECT * FROM tb_category');
-    $result->execute();
-
-    var_dump($result->fetchAll(\PDO::FETCH_ASSOC));
+    include "/application/src/View/bootstrap.php";
+    include "/application/src/View/navbar.php";
     include "/application/src/View/{$viewName}.php";
   }
 }
