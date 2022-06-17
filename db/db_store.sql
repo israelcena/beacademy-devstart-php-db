6cb2fe363e4ac2b80f4a9713892d8a59cbc62fa0
Israel Cena <israelsjm@gmail.com>
2022-06-17 16:30:08 -0300

    translate for english category_id

--- db/db_store.sql
+++ db/db_store.sql
@@ -13,7 +13,7 @@ CREATE TABLE tb_product (
   value FLOAT(5,2) NOT NULL,
   quantity INTEGER(100)  NOT NULL,
   created_at DATETIME NOT NULL,
-  categoria_id INT(11) NOT NULL
+  category_id INT(11) NOT NULL
 );
 INSERT INTO tb_category (name, description)
 VALUES(
@@ -28,7 +28,7 @@ VALUES(
     'acessorios',
     'acessorios de tecnologia e acessorios em geral'
   );
-INSERT INTO tb_product (name, description, photo, value, quantity, created_at, categoria_id) 
+INSERT INTO tb_product (name, description, photo, value, quantity, created_at, category_id) 
 VALUES ('mouse', 'mouse rgb', 'https://images-americanas.b2w.io/produtos/3057334749/imagens/mouse-c-fio-optico-mo300-multilaser/3057334749_1_large.jpg', 30.50, 10, '2021-12-31 23:59:59', 1), 
 ('teclado', 'teclado rgb', 'https://images-americanas.b2w.io/produtos/5008508351/imagens/teclado-multimedia-usb-pto-kross/5008508351_1_xlarge.jpg', 20.50, 10, '2021-11-21 03:59:59', 1),
 ('caixa de som', 'caixa de som rgb', 'https://images-americanas.b2w.io/produtos/4941332293/imagens/caixa-de-som-gamer-p2-usb-stereo-2-0-15w-rms-led-rgb-sp330-multilaser/4941332293_1_large.jpg', 32.50, 2, '2020-12-31 13:59:59', 1);
