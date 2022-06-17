1b55febb40aa7105973848af3e48bd1e47d72532
Israel Cena <israelsjm@gmail.com>
2022-06-17 12:09:51 -0300

    include list data

--- src/View/category/list.php
+++ src/View/category/list.php
@@ -1,1 +1,26 @@
+
+
 <h1>Listar Categorias</h1>
+
+<table class="table table-hover table-striped">
+  <thead class="table-dark">
+    <tr>
+      <th>#</th>
+      <th>Nome</th>
+      <th>Descrição</th>
+    </tr>
+  </thead>
+
+  <tbody>
+    <?php
+    $categories = $data->fetchAll(\PDO::FETCH_ASSOC);
+    foreach ($categories as $category) {
+      echo "<tr>"
+        . "<td>{$category['id']}</td>"
+        . "<td>{$category['name']}</td>"
+        . "<td>{$category['description']}</td>"
+        . "</tr>";
+    }
+    ?>
+  </tbody>
+</table>
