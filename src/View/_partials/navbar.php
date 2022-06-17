ce913040e1f7b691caf1cbced79ebf2b0db27a76
Israel Cena <israelsjm@gmail.com>
2022-06-17 14:51:42 -0300

    rework navbar

--- src/View/_partials/navbar.php
+++ src/View/_partials/navbar.php
@@ -1,43 +1,28 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark py-2">
   <div class="container">
     <a class="navbar-brand" href="#">E-Store</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0 gap-4">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/">Home</a>
+          <a class="nav-link <?php echo $_SERVER['REQUEST_URI'] == "/" ?  "active" : ""; ?>" aria-current="page" href="/">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/produtos">Listar Todos Produtos</a>
+          <a class="nav-link <?php echo $_SERVER['REQUEST_URI'] == "/produtos" ?  "active" : ""; ?>" href="/produtos">Produtos</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/categorias">Listar Todas Categorias</a>
+          <a class="nav-link <?php echo $_SERVER['REQUEST_URI'] == "/categorias" ?  "active" : ""; ?>" href="/categorias">Categorias</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="/produtos" id="navbarDropdownProdutos" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Produtos
+            Cadastrar
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdownProdutos">
-            <li><a class="dropdown-item" href="/produtos/novo">Cadastrar produtos</a></li>
-            <li><a class="dropdown-item" href="/produtos/editar">Editar Produtos</a></li>
-            <li><a class="dropdown-item" href="/produtos/deletar">Remover Produtos</a></li>
+            <li><a class="dropdown-item" href="/produtos/novo">Produtos</a></li>
+            <li><a class="dropdown-item" href="/categorias/novo">Categorias</a></li>
           </ul>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="/categorias" id="navbarDropdownCategorias" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Categorias
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdownCategorias">
-            <li><a class="dropdown-item" href="/categorias/novo">Cadastrar categoria</a></li>
-            <li><a class="dropdown-item" href="/categorias/editar">Editar categoria</a></li>
-            <li><a class="dropdown-item" href="/categorias/deletar">Remover categoria</a></li>
-          </ul>
-        </li>
-
-        <li class="nav-item">
-          <a class="nav-link disabled">Painel Administrativo</a>
         </li>
       </ul>
       <form class="d-flex" role="search">
