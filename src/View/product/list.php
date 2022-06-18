eb9eb5f4ba7a72dc28bacf8e2867473c5f654ca0
Israel Cena <israelsjm@gmail.com>
2022-06-17 23:55:16 -0300

    include buttons action

--- src/View/product/list.php
+++ src/View/product/list.php
@@ -1,5 +1,13 @@
 <div class="py-5 bg-light">
   <div class="container">
+    <div class="row">
+      <div class="col-3 mb-4">
+        <div class="btn-group" role="group" aria-label="Btn Actions">
+          <a href="/produtos/novo" type="button" class="btn btn-outline-primary">Cadastrar</a>
+          <a href="/produtos/relatorio" type="button" class="btn btn-outline-primary">Relatorio</a>
+        </div>
+      </div>
+    </div>
     <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
       <?php foreach ($data as $product) {
         extract($product);
